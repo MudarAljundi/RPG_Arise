64574fddd32b91cea4ea53dc826c315b6ca919c7
Alex Clay - Mudar Aljundi <alexanderclay9@gmail.com>
2017-05-03 01:50:53 -0400

    Still test stuffs

---
+++ Assets/BillboardSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour {
+
+	private Transform myCameraTransform;
+	private Transform myTransform;
+	public bool lockXAxis = true;
+
+	// Use this for initialization
+	private void Start() {
+		myTransform = transform;
+		myCameraTransform = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	private void LateUpdate() {
+
+		myTransform.forward = myCameraTransform.forward;
+
+		if (lockXAxis == true) {
+
+			myTransform.rotation = Quaternion.Euler(0, myTransform.rotation.eulerAngles.y, myTransform.rotation.eulerAngles.z);
+		}
+	}
+}
---
+++ Assets/RigidbodyRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyRotate : MonoBehaviour {
+
+	public Vector3 rotateDegreesPerSecond;
+
+	private Rigidbody myRigidbody;
+
+	private void Start() {
+		myRigidbody = GetComponent<Rigidbody>();
+	} 
+	private void FixedUpdate () {
+
+		myRigidbody.MoveRotation(myRigidbody.rotation * Quaternion.Euler(rotateDegreesPerSecond * Time.fixedDeltaTime));
+	}
+}
