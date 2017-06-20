2d5492a5089d2208664c389238b8382c3da91f4f
Alex Clay - Mudar Aljundi <alexanderclay9@gmail.com>
2017-06-20 15:42:59 -0400

    Started creating a simpler, grid level

---
+++ Assets/AI_Patrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Patrol : MonoBehaviour {
+
+	public Transform[] points;
+
+	private Vector3 destination;
+
+	private int currentPatrolIndex = 0;
+	private NavMeshAgent myNevMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+		myNevMeshAgent = GetComponent<NavMeshAgent>();
+
+		destination = points[0].position;
+
+		myNevMeshAgent.SetDestination(destination);
+	}
+	
+	void Update () {
+		if (Vector3.Distance(transform.position, destination) < 0.5f) {
+
+		}
+	}
+}
